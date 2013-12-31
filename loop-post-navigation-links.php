248134eb454ff1a22b27fd1b7b0c295a0d1a61fc
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-31 21:49:07 +0000

    * Update to v2.5 * Support looping through any taxonomy (not just categories)   * Add $taxonomy arg to nav functions (default is 'categories')   * Rename arg $in_same_cat to $in_same_term   * Rename arg $excluded_categories to $excluded_terms * Add function c2c_get_adjacent_or_loop_post_link() as non-echoing version of c2c_adjacent_or_loop_post_link() * Add function c2c_get_next_or_loop_post_link() as non-echoing version of c2c_next_or_loop_post_link() * Add function c2c_get_previous_or_loop_post_link() as non-echoing version of c2c_previous_or_loop_post_link() * Add action 'c2c_get_next_or_loop_post_link' * Add action 'c2c_get_previous_or_loop_post_link' * Add action 'c2c_get_adjacent_or_loop_post_link' * Add filter 'c2c_next_or_loop_post_link_get' * Add filter 'c2c_previous_or_loop_post_link_get' * Add unit tests * Adjust all existing do_action() calls to send an additional arg * Minor re-syncing with adjacent_post_link() * Improve phpDoc formatting (spacing) * Note compatibility through WP 3.8+ * Drop compatibility with versions of WP older than 3.6 * Update copyright date (2014) * Change donate link * Minor readme.txt tweaks (mostly spacing) * Add banner
    git-svn-id: https://plugins.svn.wordpress.org/loop-post-navigation-links/trunk@830936 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- loop-post-navigation-links.php
+++ loop-post-navigation-links.php
@@ -2,11 +2,11 @@
 /**
  * @package Loop_Post_Navigation_Links
  * @author Scott Reilly
- * @version 2.0
+ * @version 2.5
  */
 /*
 Plugin Name: Loop Post Navigation Links
-Version: 2.0
+Version: 2.5
 Plugin URI: http://coffee2code.com/wp-plugins/loop-post-navigation-links/
 Author: Scott Reilly
 Author URI: http://coffee2code.com/
@@ -14,18 +14,18 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Description: Template tags (for single.php) to create post navigation loop (previous to first post is last post; next/after last post is first post).
 
-Compatible with WordPress 2.6 through 3.5+.
+Compatible with WordPress 3.6 through 3.8+.
 
 =>> Read the accompanying readme.txt file for instructions and documentation.
 =>> Also, visit the plugin's homepage for additional information and updates.
-=>> Or visit: http://wordpress.org/extend/plugins/loop-post-navigation-links/
+=>> Or visit: http://wordpress.org/plugins/loop-post-navigation-links/
 
 TODO
 	* Switch adjacent_or_loop_post_link() away from using global $post and use get_post() instead. (Drop pre-3.5 support in doing so)
 */
 
 /*
-	Copyright (c) 2008-2013 by Scott Reilly (aka coffee2code)
+	Copyright (c) 2008-2014 by Scott Reilly (aka coffee2code)
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
@@ -34,7 +34,7 @@ TODO
 
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
@@ -47,24 +47,69 @@ defined( 'ABSPATH' ) or die();
 global $c2c_loop_navigation_find;
 $c2c_loop_navigation_find = false;
 
+
+if ( ! function_exists( 'c2c_get_next_or_loop_post_link' ) ) :
+/**
+ * Gets next post link that is adjacent to the current post, or if none, then
+ * the first post in the series.
+ *
+ * @since 2.5
+ *
+ * @param string       $format         Optional. Link anchor format. Default is '%link &raquo;'.
+ * @param string       $link           Optional. Link permalink format. Default is '%title'.
+ * @param bool         $in_same_term   Optional. Whether link should be in a same taxonomy term. Default is false.
+ * @param array|string $excluded_terms Optional. Array or comma-separated list of excluded term IDs. Default is ''.
+ * @param string       $taxonomy       Optional. Taxonomy, if $in_same_term is true. Default 'category'.
+ * @return void
+ */
+function c2c_get_next_or_loop_post_link( $format='%link &raquo;', $link='%title', $in_same_term = false, $excluded_terms = '', $taxonomy = 'category' ) {
+	return c2c_get_adjacent_or_loop_post_link( $format, $link, $in_same_term, $excluded_terms, false, $taxonomy );
+}
+add_action( 'c2c_get_next_or_loop_post_link', 'c2c_get_next_or_loop_post_link', 10, 5 );
+endif;
+
+
 if ( ! function_exists( 'c2c_next_or_loop_post_link' ) ) :
 /**
- * Display next post link that is adjacent to the current post, or if none, then
+ * Displays next post link that is adjacent to the current post, or if none, then
  * the first post in the series.
  *
  * @since 2.0
  *
- * @param string $format (optional) Link anchor format. Default is '%link &raquo;'.
- * @param string $link (optional) Link permalink format. Default is '%title'.
- * @param bool $in_same_cat (optional) Whether link should be in same category. Default is false.
- * @param string $excluded_categories (optional) Excluded categories IDs. Default is ''.
+ * @param string       $format         Optional. Link anchor format. Default is '%link &raquo;'.
+ * @param string       $link           Optional. Link permalink format. Default is '%title'.
+ * @param bool         $in_same_term   Optional. Whether link should be in a same taxonomy term. Default is false.
+ * @param array|string $excluded_terms Optional. Array or comma-separated list of excluded term IDs. Default is ''.
+ * @param string       $taxonomy       Optional. Taxonomy, if $in_same_term is true. Default 'category'.
  * @return void
  */
-function c2c_next_or_loop_post_link( $format='%link &raquo;', $link='%title', $in_same_cat = false, $excluded_categories = '' ) {
-	c2c_adjacent_or_loop_post_link( $format, $link, $in_same_cat, $excluded_categories, false );
+function c2c_next_or_loop_post_link( $format='%link &raquo;', $link='%title', $in_same_term = false, $excluded_terms = '', $taxonomy = 'category' ) {
+	c2c_adjacent_or_loop_post_link( $format, $link, $in_same_term, $excluded_terms, false, $taxonomy );
 }
-add_action( 'c2c_next_or_loop_post_link', 'c2c_next_or_loop_post_link', 10, 4 );
+add_action( 'c2c_next_or_loop_post_link', 'c2c_next_or_loop_post_link', 10, 5 );
 endif;
+
+
+if ( ! function_exists( 'c2c_get_previous_or_loop_post_link' ) ) :
+/**
+ * Display previous post link that is adjacent to the current post, or if none,
+ * then the last post in the series.
+ *
+ * @since 2.5
+ *
+ * @param string       $format         Optional. Link anchor format. Default is '&laquo; %link'.
+ * @param string       $link           Optional. Link permalink format. Default is '%title'.
+ * @param bool         $in_same_term   Optional. Whether link should be in a same taxonomy term. Default is false.
+ * @param array|string $excluded_terms Optional. Array or comma-separated list of excluded term IDs. Default is ''.
+ * @param string       $taxonomy       Optional. Taxonomy, if $in_same_term is true. Default 'category'.
+ * @return void
+ */
+function c2c_get_previous_or_loop_post_link( $format='&laquo; %link', $link='%title', $in_same_term = false, $excluded_terms = '', $taxonomy = 'category' ) {
+	return c2c_get_adjacent_or_loop_post_link( $format, $link, $in_same_term, $excluded_terms, true, $taxonomy );
+}
+add_action( 'c2c_previous_or_loop_post_link', 'c2c_previous_or_loop_post_link', 10, 5 );
+endif;
+
 
 if ( ! function_exists( 'c2c_previous_or_loop_post_link' ) ) :
 /**
@@ -73,42 +118,47 @@ if ( ! function_exists( 'c2c_previous_or_loop_post_link' ) ) :
  *
  * @since 2.0
  *
- * @param string $format (optional) Link anchor format. Default is '&laquo; %link'.
- * @param string $link (optional) Link permalink format. Default is '%title'.
- * @param bool $in_same_cat (optional) Whether link should be in same category. Default is false.
- * @param string $excluded_categories (optional) Excluded categories IDs. Default is ''.
+ * @param string       $format         Optional. Link anchor format. Default is '&laquo; %link'.
+ * @param string       $link           Optional. Link permalink format. Default is '%title'.
+ * @param bool         $in_same_term   Optional. Whether link should be in a same taxonomy term. Default is false.
+ * @param array|string $excluded_terms Optional. Array or comma-separated list of excluded term IDs. Default is ''.
+ * @param string       $taxonomy       Optional. Taxonomy, if $in_same_term is true. Default 'category'.
  * @return void
  */
-function c2c_previous_or_loop_post_link( $format='&laquo; %link', $link='%title', $in_same_cat = false, $excluded_categories = '' ) {
-	c2c_adjacent_or_loop_post_link( $format, $link, $in_same_cat, $excluded_categories, true );
+function c2c_previous_or_loop_post_link( $format='&laquo; %link', $link='%title', $in_same_term = false, $excluded_terms = '', $taxonomy = 'category' ) {
+	c2c_adjacent_or_loop_post_link( $format, $link, $in_same_term, $excluded_terms, true, $taxonomy );
 }
-add_action( 'c2c_previous_or_loop_post_link', 'c2c_previous_or_loop_post_link', 10, 4 );
+add_action( 'c2c_previous_or_loop_post_link', 'c2c_previous_or_loop_post_link', 10, 5 );
 endif;
 
-if ( ! function_exists( 'c2c_adjacent_or_loop_post_link' ) ) :
+
+if ( ! function_exists( 'c2c_get_adjacent_or_loop_post_link' ) ) :
 /**
- * Display adjacent post link or the post link for the post at the opposite end of the series.
+ * Gets adjacent post link or the post link for the post at the opposite end of the series.
  *
  * Can be either next post link or previous.
  *
- * @param string $format Link anchor format.
- * @param string $link Link permalink format.
- * @param bool $in_same_cat (optional) Whether link should be in same category. Default is false.
- * @param string $excluded_categories (optional) Excluded categories IDs. Default is ''.
- * @param bool $previous (optional) Whether to display link to previous post. Default is true.
+ * @since 2.5
+ *
+ * @param string       $format         Link anchor format.
+ * @param string       $link           Link permalink format.
+ * @param bool         $in_same_term   Optional. Whether link should be in a same taxonomy term. Default is false.
+ * @param array|string $excluded_terms Optional. Array or comma-separated list of excluded term IDs. Default is ''.
+ * @param bool         $previous       Optional. Whether to display link to previous or next post. Default is true.
+ * @param string       $taxonomy       Optional. Taxonomy, if $in_same_term is true. Default 'category'.
  * @return void
  */
-function c2c_adjacent_or_loop_post_link( $format, $link, $in_same_cat = false, $excluded_categories = '', $previous = true ) {
-	if ( $previous && is_attachment() && is_object( $GLOBALS['post'] ) )
-		$post = get_post( $GLOBALS['post']->post_parent );
+function c2c_get_adjacent_or_loop_post_link( $format, $link, $in_same_term = false, $excluded_terms = '', $previous = true, $taxonomy = 'category' ) {
+	if ( $previous && is_attachment() )
+		$post = get_post( get_post()->post_parent );
 	else
-		$post = get_adjacent_post( $in_same_cat, $excluded_categories, $previous );
+		$post = get_adjacent_post( $in_same_term, $excluded_terms, $previous, $taxonomy );
 
 	// START The only modification of adjacent_post_link() -- get the last/first post if there isn't a legitimate previous/next post
 	if ( ! $post ) {
 		global $c2c_loop_navigation_find;
 		$c2c_loop_navigation_find = true;
-		$post = get_adjacent_post( $in_same_cat, $excluded_categories, $previous );
+		$post = get_adjacent_post( $in_same_term, $excluded_terms, $previous, $taxonomy );
 		$c2c_loop_navigation_find = false;
 	}
 	// END modification
@@ -142,11 +192,50 @@ function c2c_adjacent_or_loop_post_link( $format, $link, $in_same_cat = false, $
 	// Deprecated as of v2.0. Here temporarily for backwards compatibility.
 	$output = apply_filters( "{$adjacent}_or_loop_post_link", $output, $format, $link, $post );
 
-	// Apply custom filters and echo
-	echo apply_filters( "c2c_{$adjacent}_or_loop_post_link_output", $output, $format, $link, $post );
+	// Apply custom filters and return
+	return apply_filters( "c2c_{$adjacent}_or_loop_post_link_get", $output, $format, $link, $post, $in_same_term, $excluded_terms, $taxonomy );
 }
-add_action( 'c2c_adjacent_or_loop_post_link', 'c2c_previous_or_loop_post_link', 10, 5 );
+add_action( 'c2c_get_adjacent_or_loop_post_link', 'c2c_get_adjacent_or_loop_post_link', 10, 6 );
 endif;
+
+
+if ( ! function_exists( 'c2c_adjacent_or_loop_post_link' ) ) :
+/**
+ * Displays adjacent post link or the post link for the post at the opposite end of the series.
+ *
+ * Can be either next post link or previous.
+ *
+ * @param string       $format         Link anchor format.
+ * @param string       $link           Link permalink format.
+ * @param bool         $in_same_term   Optional. Whether link should be in a same taxonomy term. Default is false.
+ * @param array|string $excluded_terms Optional. Array or comma-separated list of excluded term IDs. Default is ''.
+ * @param bool         $previous       Optional. Whether to display link to previous or next post. Default is true.
+ * @param string       $taxonomy       Optional. Taxonomy, if $in_same_term is true. Default 'category'.
+ * @return void
+ */
+function c2c_adjacent_or_loop_post_link( $format, $link, $in_same_term = false, $excluded_terms = '', $previous = true, $taxonomy = 'category' ) {
+	if ( $previous && is_attachment() )
+		$post = get_post( get_post()->post_parent );
+	else
+		$post = get_adjacent_post( $in_same_term, $excluded_terms, $previous, $taxonomy );
+
+	$adjacent = $previous ? 'previous' : 'next';
+
+	// Apply custom filters and echo
+	echo apply_filters(
+		"c2c_{$adjacent}_or_loop_post_link_output",
+		c2c_get_adjacent_or_loop_post_link( $format, $link, $in_same_term, $excluded_terms, $previous, $taxonomy ),
+		$format,
+		$link,
+		$post,
+		$in_same_term,
+		$excluded_terms,
+		$taxonomy
+	);
+}
+add_action( 'c2c_adjacent_or_loop_post_link', 'c2c_previous_or_loop_post_link', 10, 6 );
+endif;
+
 
 if ( ! function_exists( 'c2c_modify_nextprevious_post_where' ) ) :
 /**
@@ -154,16 +243,16 @@ if ( ! function_exists( 'c2c_modify_nextprevious_post_where' ) ) :
  *
  * Can be either next post link or previous.
  *
- * @param string $where SQL WHERE clause generated by WordPress
- * @param string $link Link permalink format.
- * @param bool $in_same_cat (optional) Whether link should be in same category. Default is false.
- * @param string $excluded_categories (optional) Excluded categories IDs. Default is ''.
- * @param bool $previous (optional) Whether to display link to previous post. Default is true.
+ * @param string       $where          SQL WHERE clause generated by WordPress
+ * @param string       $link           Link permalink format.
+ * @param bool         $in_same_term   Optional. Whether link should be in a same taxonomy term. Default is false.
+ * @param array|string $excluded_terms Optional. Array or comma-separated list of excluded term IDs. Default is ''.
+ * @param bool         $previous       Optional. Whether to display link to previous or next post. Default is true.
  * @return void
  */
 function c2c_modify_nextprevious_post_where( $where ) {
 	// The incoming WHERE statement generated by WordPress is a condition for the date, relative to the current
-	//	post's date.  To find the post we want, we just need to get rid of that condition (which is the first) and retain the others.
+	//	post's date. To find the post we want, we just need to get rid of that condition (which is the first) and retain the others.
 	if ( $GLOBALS['c2c_loop_navigation_find'] )
 		$where = preg_replace( '/WHERE (.+) AND/imsU', 'WHERE', $where );
 	return $where;
@@ -189,9 +278,9 @@ if ( ! function_exists( 'next_or_loop_post_link' ) ) :
 	 * @since 1.0
 	 * @deprecated 2.0 Use c2c_next_or_loop_post_link() instead
 	 */
-	function next_or_loop_post_link( $format='%link &raquo;', $link='%title', $in_same_cat = false, $excluded_categories = '' ) {
+	function next_or_loop_post_link( $format='%link &raquo;', $link='%title', $in_same_term = false, $excluded_terms = '' ) {
 		_deprecated_function( 'next_or_loop_post_link', '2.0', 'c2c_next_or_loop_post_link' );
-		return c2c_next_or_loop_post_link( $format, $link, $in_same_cat, $excluded_categories );
+		return c2c_next_or_loop_post_link( $format, $link, $in_same_term, $excluded_terms );
 	}
 endif;
 
@@ -203,9 +292,9 @@ if ( ! function_exists( 'previous_or_loop_post_link' ) ) :
 	 * @since 1.0
 	 * @deprecated 2.0 Use c2c_previous_or_loop_post_link() instead
 	 */
-	function previous_or_loop_post_link( $format='&laquo; %link', $link='%title', $in_same_cat = false, $excluded_categories = '' ) {
+	function previous_or_loop_post_link( $format='&laquo; %link', $link='%title', $in_same_term = false, $excluded_terms = '' ) {
 		_deprecated_function( 'previous_or_loop_post_link', '2.0', 'c2c_previous_or_loop_post_link' );
-		return c2c_previous_or_loop_post_link( $format, $link, $in_same_cat, $excluded_categories );
+		return c2c_previous_or_loop_post_link( $format, $link, $in_same_term, $excluded_terms );
 	}
 endif;
 
@@ -217,8 +306,8 @@ if ( ! function_exists( 'adjacent_or_loop_post_link' ) ) :
 	 * @since 1.0
 	 * @deprecated 2.0 Use c2c_adjacent_or_loop_post_link() instead
 	 */
-	function adjacent_or_loop_post_link( $format, $link, $in_same_cat = false, $excluded_categories = '', $previous = true ) {
+	function adjacent_or_loop_post_link( $format, $link, $in_same_term = false, $excluded_terms = '', $previous = true ) {
 		_deprecated_function( 'adjacent_or_loop_post_link', '2.0', 'c2c_adjacent_or_loop_post_link' );
-		return c2c_adjacent_or_loop_post_link( $format, $link, $in_same_cat, $excluded_categories, $previous );
+		return c2c_adjacent_or_loop_post_link( $format, $link, $in_same_term, $excluded_terms, $previous );
 	}
 endif;
--- readme.txt
+++ readme.txt
@@ -1,13 +1,12 @@
 === Loop Post Navigation Links ===
 Contributors: coffee2code
-Donate link: http://coffee2code.com/donate
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6ARCFJ9TX3522
 Tags: posts, navigation, links, next, previous, portfolio, previous_post_link, next_post_link, coffee2code
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Requires at least: 2.6
-Tested up to: 3.5
-Stable tag: 2.0
-Version: 2.0
+Requires at least: 3.6
+Tested up to: 3.8
+Stable tag: 2.5
 
 Template tags (for use in single.php) to create post navigation loop (previous to first post is last post; next/after last post is first post).
 
@@ -22,7 +21,7 @@ The function `c2c_previous_or_loop_post_link()` is identical to WordPress's `pre
 
 Useful for providing a looping link of posts, such as for a portfolio, or to continually present pertinent posts for visitors to continue reading.
 
-Links: [Plugin Homepage](http://coffee2code.com/wp-plugins/loop-post-navigation-links/) | [Plugin Directory Page](http://wordpress.org/extend/plugins/loop-post-navigation-links/) | [Author Homepage](http://coffee2code.com)
+Links: [Plugin Homepage](http://coffee2code.com/wp-plugins/loop-post-navigation-links/) | [Plugin Directory Page](http://wordpress.org/plugins/loop-post-navigation-links/) | [Author Homepage](http://coffee2code.com)
 
 
 == Installation ==
@@ -34,42 +33,48 @@ Links: [Plugin Homepage](http://coffee2code.com/wp-plugins/loop-post-navigation-
 
 == Template Tags ==
 
-The plugin provides two template tags for use in your single-post theme templates.
+The plugin provides four template tags for use in your single-post theme templates.
 
 = Functions =
 
-* `function c2c_next_or_loop_post_link( $format='%link &raquo;', $link='%title', $in_same_cat = false, $excluded_categories = '' )`
-Like WordPress's `next_post_link()`, this function displays a link to the next chronological post (among all published posts, those in the same category, or those not in certain categories).  Unlink `next_post_link()`, when on the last post in the sequence this function will link back to the first post in the sequence, creating a circular loop.
+* `function c2c_next_or_loop_post_link( $format='%link &raquo;', $link='%title', $in_same_term = false, $excluded_terms = '', $taxonomy = 'category' )`
+Like WordPress's `next_post_link()`, this function displays a link to the next chronological post (among all published posts, those in the same category, or those not in certain categories). Unlink `next_post_link()`, when on the last post in the sequence this function will link back to the first post in the sequence, creating a circular loop.
 
-* `function c2c_previous_or_loop_post_link( $format='&laquo; %link', $link='%title', $in_same_cat = false, $excluded_categories = '' )`
-Like WordPress's `previous_post_link()`, this function displays a link to the previous chronological post (among all published posts, those in the same category, or those not in certain categories).  Unlink `previous_post_link()`, when on the first post in the sequence this function will link to the last post in the sequence, creating a circular loop.
+* `function c2c_get_next_or_loop_post_link( $format='%link &raquo;', $link='%title', $in_same_term = false, $excluded_terms = '', $taxonomy = 'category' )`
+Like `c2c_next_or_loop_post_link(), but returns the value without echoing it.
+
+* `function c2c_previous_or_loop_post_link( $format='&laquo; %link', $link='%title', $in_same_term = false, $excluded_terms = '', $taxonomy = 'category' )`
+Like WordPress's `previous_post_link()`, this function displays a link to the previous chronological post (among all published posts, those in the same category, or those not in certain categories). Unlink `previous_post_link()`, when on the first post in the sequence this function will link to the last post in the sequence, creating a circular loop.
+
+* `function c2c_get_previous_or_loop_post_link( $format='&laquo; %link', $link='%title', $in_same_term = false, $excluded_terms = '', $taxonomy = 'category' )`
+Like `c2c_get_previous_or_loop_post_link(), but returns the value without echoing it.
 
 = Arguments =
 
 * `$format`
-(optional) A percent-substitution string indicating the format of the entire output string.  Use <code>%link</code> to represent the next/previous post being linked, or <code>%title</code> to represent the title of the next/previous post.
+(optional) A percent-substitution string indicating the format of the entire output string. Use <code>%link</code> to represent the next/previous post being linked, or <code>%title</code> to represent the title of the next/previous post.
 
 * `$link`
-(optional) A percent-substitution string indicating the format of the link itself that gets created for the next/previous post.  Use <code>%link</code> to represent the next/previous post being linked, or <code>%title</code> to represent the title of the next/previous post.
+(optional) A percent-substitution string indicating the format of the link itself that gets created for the next/previous post. Use <code>%link</code> to represent the next/previous post being linked, or <code>%title</code> to represent the title of the next/previous post.
 
-* `$in_same_cat`
-(optional) A boolean value (either true or false) indicating if the next/previous post should be in the current post's same category.
+* `$in_same_term`
+(optional) A boolean value (either true or false) indicating if the next/previous post should be in the current post's same taxonomy term.
 
-* `$excluded_categories`
-(optional) A string of category IDs to which posts cannot belong.  Due to goofy a WP convention, the category IDs need to be joined by " and ", i.e. "14 and 15 and 31".
+* `$excluded_terms`
+(optional) An array or comma-separated string of category or term IDs to which posts cannot belong.
 
 
 == Examples ==
 
 `<div class="navigation">
-	<div class="alignleft"><?php previous_or_loop_post_link(); ?></div>
-	<div class="alignright"><?php next_or_loop_post_link(); ?></div>
+	<div class="alignleft"><?php c2c_previous_or_loop_post_link(); ?></div>
+	<div class="alignright"><?php c2c_next_or_loop_post_link(); ?></div>
 </div>`
 
 
 == Filters ==
 
-The plugin is further customizable via four hooks. Typically, this type of customization would be put into your active theme's functions.php file or used by another plugin.
+The plugin is further customizable via eight hooks. Typically, this type of customization would be put into your active theme's functions.php file or used by another plugin.
 
 = c2c_previous_or_loop_post_link_output, c2c_next_or_loop_post_link_output (filters) =
 
@@ -79,15 +84,19 @@ Example:
 
   `<?php
     // Prepend "Prev:" to previous link markup.
-    function my_custom_previous_or_loop_link_output( $output, $format, $link, $post ) {
+    function my_custom_previous_or_loop_link_output( $output, $format, $link, $post, $in_same_term, $excluded_terms, $taxonomy ) {
       return 'Prev: ' . $output;
     }
     add_filter( 'c2c_previous_or_loop_post_link_output', 'my_custom_previous_or_loop_link_output', 10, 4 );
   ?>`
 
-= c2c_previous_or_loop_post_link, c2c_next_or_loop_post_link (actions) =
+= c2c_previous_or_loop_post_link_get, c2c_next_or_loop_post_link_get (filters) =
 
-The 'c2c_previous_or_loop_post_link' and 'c2c_next_or_loop_post_link' actions allow you to use an alternative approach to safely invoke `c2c_previous_or_loop_post_link()` and `c2c_previous_or_loop_post_link()`, respectively, in such a way that if the plugin were deactivated or deleted, then your calls to the functions won't cause errors in your site.
+The 'c2c_previous_or_loop_post_link_get' and 'c2c_next_or_loop_post_link_get' filters allow you to customize the link markups generated for previous and next looping links, respectively, but in the non-echoing functions.
+
+= c2c_previous_or_loop_post_link, c2c_next_or_loop_post_link (actions), c2c_get_previous_or_loop_post_link, c2c_get_next_or_loop_post_link (filters), =
+
+The 'c2c_previous_or_loop_post_link' and 'c2c_next_or_loop_post_link' actions allow you to use an alternative approach to safely invoke `c2c_previous_or_loop_post_link()` and `c2c_next_or_loop_post_link()`, respectively, in such a way that if the plugin were deactivated or deleted, then your calls to the functions won't cause errors in your site. The 'c2c_get_previous_or_loop_post_link' and 'c2c_get_next_or_loop_post_link' filters do the same for the non-echoing `c2c_previous_or_loop_post_link()` and `c2c_next_or_loop_post_link()`.
 
 Arguments:
 
@@ -106,13 +115,37 @@ Do:
 
 == Changelog ==
 
+= 2.5 (2013-12-31) =
+* Support looping through any taxonomy (not just categories)
+  * Add $taxonomy arg to nav functions (default is 'categories')
+  * Rename arg $in_same_cat to $in_same_term
+  * Rename arg $excluded_categories to $excluded_terms
+* Add function c2c_get_adjacent_or_loop_post_link() as non-echoing version of c2c_adjacent_or_loop_post_link()
+* Add function c2c_get_next_or_loop_post_link() as non-echoing version of c2c_next_or_loop_post_link()
+* Add function c2c_get_previous_or_loop_post_link() as non-echoing version of c2c_previous_or_loop_post_link()
+* Add action 'c2c_get_next_or_loop_post_link'
+* Add action 'c2c_get_previous_or_loop_post_link'
+* Add action 'c2c_get_adjacent_or_loop_post_link'
+* Add filter 'c2c_next_or_loop_post_link_get'
+* Add filter 'c2c_previous_or_loop_post_link_get'
+* Add unit tests
+* Adjust all existing do_action() calls to send an additional arg
+* Minor re-syncing with adjacent_post_link()
+* Improve phpDoc formatting (spacing)
+* Note compatibility through WP 3.8+
+* Drop compatibility with versions of WP older than 3.6
+* Update copyright date (2014)
+* Change donate link
+* Minor readme.txt tweaks (mostly spacing)
+* Add banner
+
 = 2.0 =
 * Sync `adjacent_or_loop_post_link()` with most changes made to WP's `adjacent_or_post_link()`
-* * Always run output through filters
-* * Pass original $format to filters
-* * Pass $post to filters
-* * Minor code reformatting (spacing)
-* * NOTE: arguments to filters have changed
+  * Always run output through filters
+  * Pass original $format to filters
+  * Pass $post to filters
+  * Minor code reformatting (spacing)
+  * NOTE: arguments to filters have changed
 * Rename `next_or_loop_post_link()` to `c2c_next_or_loop_post_link()` (but maintain a deprecated version for backwards compatibility)
 * Rename `previous_or_loop_post_link()` to `c2c_previous_or_loop_post_link()` (but maintain a deprecated version for backwards compatibility)
 * Rename `adjacent_or_loop_post_link()` to `c2c_adjacent_or_loop_post_link()` (but maintain a deprecated version for backwards compatibility)
@@ -176,6 +209,9 @@ Do:
 
 
 == Upgrade Notice ==
+
+= 2.5 =
+Major update: added support for navigating by taxonomy; added non-echoing versions of functions, and more filters; added unit tests; noted compatibility through WP 3.8+; dropped compatibility with WP older than 3.6
 
 = 2.0 =
 Recommended major update: synced with changes made to WP; added filters; changed arguments to existing filters; renamed and deprecated all existing functions and filters; noted compatibility through WP 3.5+; and more. (All your old usage will still work, though)
